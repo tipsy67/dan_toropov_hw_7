6469eb619ce1acff110aa5434f2ccddeb8a002ec
dan.toropov <dan.toropov@gmail.com>
2024-10-24 21:55:07 +0300

    save project

--- lws/models.py
+++ lws/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(**NULLABLE)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name='')
+    description = models.TextField(**NULLABLE)
+    preview = models.ImageField(**NULLABLE)
+    lessons = models.ManyToManyField(to='Lesson', related_name='courses')
